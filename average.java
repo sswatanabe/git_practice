e451b1298f3d030355e7ac3afef365bea71e8895
sswatanabe <swatanabe@crooz.co.jp>
2015-12-10 06:47:19 +0000

    tmp

---
+++ average.java
@@ -0,0 +1,24 @@
+public class average{
+  public static void main(String[] args){
+   
+   System.out.print("数値を入力してください\n");
+   System.out.print("entと入力したら今まで入力した数を割ります\n");
+
+   double[] data;
+   double tmp;
+   int i=0;
+   while(i<256){
+     i++;
+     String command = new java.util.Scanner(System.in).nextLine();
+      switch(command){
+	case '+':
+	  System.out.print("++");
+	case '/':
+	  System.out.print("//");
+	case ';':
+	  break;
+	}
+    }
+  
+  }
+}
