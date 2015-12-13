e9d68a6e8eb298b3b60345cad5dcfe337c4bb37a
sswatanabe <swatanabe@crooz.co.jp>
2015-12-13 06:34:28 +0000

    update

--- calc.java
+++ calc.java
@@ -1,23 +1,55 @@
-public class average{
-  public static void main(String[] args){
-   
-   System.out.print("数値を入力してください\n");
-   System.out.print("entと入力したら今まで入力した数を割ります\n");
+/* 電卓みたいなプログラム
+   since:12/13
+   author:sswatanabe*/
 
-   double[] data;
-   double tmp;
+
+public class calc{
+  public static void main(String[] args){
+
+   double[] data = new double[256];
+   String[] command = new String[256];
+   double sum = 0;
    int i=0;
    while(i<256){
-     i++;
-     String command = new java.util.Scanner(System.in).nextLine();
-      switch(command){
-	case '+':
-	  System.out.print("++");
-	case '/':
-	  System.out.print("//");
-	case ';':
+
+     System.out.print("数値を入力してください\n");
+     data[i] = new java.util.Scanner(System.in).nextInt();
+
+     if(i != 0){
+      switch(command[i-1]){
+        case "+":
+          sum = sum + data[i];
+          System.out.print("++\n");
+          break;
+	case "-":
+	  sum = sum -data[i];
 	  break;
-	}
+	case "*":
+	  sum = sum * data[i];
+	  break;
+        case "/":
+          sum = sum / data[i];
+          System.out.print("//\n");
+          break;
+        case ";":
+          i = 256;
+          break;
+        }
+        }
+
+
+
+     if(i == 0){
+	sum = data[i];
+	System.out.print("現在の合計値：" + data[i] + "\n");
+     }else{
+        System.out.print("現在の合計値：" + sum + "\n");
+     }
+
+     System.out.print("次の作業を入力してください\n");
+     command[i] = new java.util.Scanner(System.in).nextLine();
+     
+      i++;
     }
   
   }
